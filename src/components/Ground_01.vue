941a71f0f1f564a5eeb56c699ef1dbb9cfa911ca
leejc <leejc831@naver.com>
2023-03-19 22:15:45 +0900

    memo 페이지 추가

---
+++ src/components/Ground_01.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ground">
+    <Header/>
+    <Memo/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./Header_01.vue";
+import Footer from "./Footer_01.vue";
+import Memo from "./Memo_01.vue";
+export default {
+  components:{Header, Footer, Memo}
+}
+</script>
+
+
---
+++ src/components/Memo_01.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="memo">
+        <ul>
+            <li v-for="(d, idx) in data" :key="idx">{{ d }}</li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        const data = [
+            "메모 1 내용",
+            "메모 2 내용",
+            "메모 3 내용",
+            "메모 4 내용"
+        ]
+
+        return {data};
+    },
+}
+</script>pt
+
+<style scroped>
+
+.memo ul {
+    list-style:none;
+    padding:15px 0;
+    margin:0;
+
+   
+}
+
+.memo li {
+    padding:5px 10px;
+    margin:5;
+}
+
+
+</style>
