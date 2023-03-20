e65ef8f6e000f9a83bec6edae9b677cd34784f43
LEEJAECHUN <leejc831@naver.com>
2023-03-20 15:00:28 +0900

    memo 페이지 추가

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <Ground/>
+  </div>
 </template>
   
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Ground from "./components/Ground_01.vue" /* eslint-disable */
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{Ground} 
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Memo_01.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="memo">
+        <div class = "act">
+            <button class="btn btn-primary" @click="add()">+ 추가</button>
+        </div>
+        <ul>
+            <li v-for="(d, idx) in state.data" :key="idx">{{ d }}</li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+import {reactive} from "vue";
+export default {
+    
+    setup() {
+        const state = reactive({
+            data : [],
+        });
+
+        const add = ()=>{
+            state.data.push("추가된 메모 내용");
+        }
+
+        return {state, add};
+    },
+}
+</script>
+
+<style scroped>
+
+.memo ul {
+    border-top:1px solid #eee;
+    list-style:none;
+    padding:15px 0;
+    margin:0;
+
+   
+}
+
+.memo li {
+    padding:15px;
+    margin:5px;
+    border:1px solid #eee;
+    
+}
+
+.act {
+    padding :10px 5px 5px 5px;
+    text-align: right;
+}
+
+
+</style>
