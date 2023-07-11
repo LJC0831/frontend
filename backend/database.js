87417b5a6203310f7b52774089b9c1e6f2ee97a2
LEEJAECHUN <leejc831@naver.com>
2023-07-11 11:10:51 +0900

    DB서버 세팅

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,20 @@
 const mariadb = require('mariadb');
+// const pool = mariadb.createPool({
+//      host: '127.0.0.1', 
+//      user:'root', 
+//      password: 'qrg258*001',
+//      connectionLimit: 3000,
+//      database:'board'
+// });
 const pool = mariadb.createPool({
-     host: '127.0.0.1', 
-     user:'root', 
-     password: 'qrg258*001',
-     connectionLimit: 3000,
-     database:'board'
+    host: 'svc.exp0.cloudtype.app', 
+    user:'root', 
+    port:'32428',
+    password: '1234',
+    connectionLimit: 3000,
+    database:'board'
 });
+
 
 module.exports = {
     async run(query){
