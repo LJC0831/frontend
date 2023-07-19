eb93d1e0a7abd1b5ee2716399f8001e921c8fb68
LEEJAECHUN <leejc831@naver.com>
2023-07-19 09:59:54 +0900

    proxy 수정

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
     devServer:{
         proxy:{
             "/api":{
-                target:"http://localhost:3000"
+                //target:"http://localhost:3000"
+                target:"https://port-0-backend-nodejs-20zynm2mlk2nnlwj.sel4.cloudtype.app/"
             }
         }
     },
