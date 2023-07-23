6cc293fbd375eab4b94fd6348da129db75dc663f
leejc <leejc831@naver.com>
2023-07-24 00:39:04 +0900

    토글처리

--- src/components/SideBar_01.vue
+++ src/components/SideBar_01.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="sidebar">
+  <div>
     <!-- 메뉴 항목들 -->
-    <div class="menu-item" @click="handleMenuClick('테스트1')">전체 글 보기</div>
-    <div class="menu-item" @click="handleMenuClick('테스트2')">문의하기</div>
-    <div class="menu-item" @click="handleMenuClick('테스트3')">Q&A</div>
+    <button @click="toggleSidebar">{{ sidebarOpen ? '메뉴열기' : '메뉴닫기' }}</button>
+    <div class="sidebar" :class="{ 'open': sidebarOpen }">
+        <div class="menu-item"  v-if="!sidebarOpen" @click="handleMenuClick('테스트1')">전체 글 보기</div>
+        <div class="menu-item"  v-if="!sidebarOpen" @click="handleMenuClick('테스트2')">문의하기</div>
+        <div class="menu-item"  v-if="!sidebarOpen" @click="handleMenuClick('테스트3')">Q&A</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+        sidebarOpen: false, // 사이드바의 초기 상태는 열린 상태로 설정
+        };
+    },
   methods: {
+    toggleSidebar() {
+      this.sidebarOpen = !this.sidebarOpen; // 상태를 토글하여 사이드바 접기/열기
+    },
     handleMenuClick(menuName) {
       alert(`선택한 메뉴: ${menuName}`);
       // 여기에 선택한 메뉴에 따른 추가 로직을 작성할 수 있습니다.
@@ -26,6 +37,10 @@ export default {
   background-color: #f0f0f0;
   padding: 10px;
   flex: 0 0 200px; /* 사이드바 너비 설정 */
+  transition: width 0.3s ease; /* 사이드바 접고/열 때 애니메이션 적용 */
+}
+.open {
+  width: 0; /* 사이드바 접을 때 너비를 0으로 설정하여 감추기 */
 }
 
 .menu-item {
