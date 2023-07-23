2e31f716e0bc1bfcc69779b611c18fba95f17ef0
leejc <leejc831@naver.com>
2023-07-24 00:23:38 +0900

    세션 1시간처리

---
+++ src/helpers.js
@@ -0,0 +1,31 @@
+// helpers.js 파일 생성
+// 토큰 저장 함수
+/* eslint-disable */
+export function saveTokenToLocalStorage(token) {
+    localStorage.setItem('token', token);
+  }
+  
+  // 토큰 삭제 함수
+  export function removeTokenFromLocalStorage() {
+    localStorage.removeItem('token');
+  }
+  
+  // 토큰 유효기간 확인 함수
+  export function checkTokenExpiration() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        const expirationTime = decodedToken.exp * 1000; // 토큰 만료 시간 (밀리초 단위)
+        const currentTime = new Date().getTime(); // 현재 시간 (밀리초 단위)
+  
+        // 토큰 만료 시간이 현재 시간보다 작다면 토큰을 삭제
+        if (expirationTime < currentTime) {
+          removeTokenFromLocalStorage();
+        }
+      } catch (error) {
+        // 토큰 디코딩에 실패하면 토큰 삭제
+        removeTokenFromLocalStorage();
+      }
+    }
+  }
--- src/store.js
+++ src/store.js
@@ -1,4 +1,8 @@
+// store.js 파일 수정
+/* eslint-disable */
 import { createStore } from 'vuex';
+import jwtDecode from 'jwt-decode'; // jwt 디코딩 라이브러리
+import { saveTokenToLocalStorage, removeTokenFromLocalStorage, checkTokenExpiration } from './helpers';
 
 const store = createStore({
   state: {
@@ -7,9 +11,11 @@ const store = createStore({
   mutations: {
     setToken(state, token) {
       state.token = token;
+      saveTokenToLocalStorage(token); // 토큰 저장
     },
     clearToken(state) {
       state.token = null;
+      removeTokenFromLocalStorage(); // 토큰 삭제
     }
   },
   actions: {
@@ -24,5 +30,10 @@ const store = createStore({
     getToken: state => state.token
   }
 });
+
+// 토큰 유효기간 확인 타이머 설정
+setInterval(() => {
+  checkTokenExpiration();
+}, 300000); // 5분마다 실행 (1000밀리초 * 60초 = 1분)
 
 export default store;
