db70c38b75f57ac56b14ce69d0cf277b16e3d0ce
leejc <leejc831@naver.com>
2023-07-26 23:56:35 +0900

    디자인수정

--- src/components/Footer_01.vue
+++ src/components/Footer_01.vue
@@ -1,11 +1,15 @@
 <template>
-    <footer><b>※ 내용 클릭 시 내용수정 가능</b><br/> 2023 memo app. All rights reserved</footer>
-</template>
-
-<style scoped>
-    footer{
-        background: #f7f7f7;
-        border-top: 1px solid #eee;
-        padding:25px;
-    }
-</style>
+    <footer class="footer-container"><b>※ 내용 클릭 시 내용수정 가능</b> 2023 memo app. All rights reserved</footer>
+  </template>
+  
+  <style scoped>
+  .footer-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #f7f7f7;
+    border-top: 1px solid #eee;
+    padding: 25px;
+  }
+  </style>
