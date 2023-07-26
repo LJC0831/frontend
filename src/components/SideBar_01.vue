208c37d00ae0f81e1f7cdb5eea91efb97b86dd31
leejc <leejc831@naver.com>
2023-07-27 00:31:57 +0900

    디자인수정

--- src/components/SideBar_01.vue
+++ src/components/SideBar_01.vue
@@ -20,10 +20,10 @@
 export default {
   data() {
     return {
-      sidebarOpen: false,
+      sidebarOpen: true,
       menus: [
-        { name: '테스트1', label: '전체 글 보기' },
-        { name: '테스트2', label: '문의하기' },
+        { name: '테스트1', label: '소개 글 보기' },
+        { name: '테스트2', label: '글 작성' },
         { name: '테스트3', label: 'Q&A' },
       ],
     };
@@ -31,6 +31,19 @@ export default {
   methods: {
     toggleSidebar() {
       this.sidebarOpen = !this.sidebarOpen;
+      // 토글 시 사이드바의 z-index 변경
+      this.$nextTick(() => {
+        const sidebarElement = document.querySelector('.sidebar');
+        if (this.sidebarOpen) {
+          sidebarElement.style.zIndex = '1000';
+          // 사이드바가 열렸을 때 포인터 이벤트를 받을 수 있도록 설정
+          sidebarElement.style.pointerEvents = 'auto';
+        } else {
+          sidebarElement.style.zIndex = '1';
+          // 사이드바가 닫혔을 때 포인터 이벤트를 받지 않도록 설정
+          sidebarElement.style.pointerEvents = 'none';
+        }
+      });
     },
     handleMenuClick(menuName) {
       alert(`선택한 메뉴: ${menuName}`);
@@ -44,22 +57,37 @@ export default {
   display: flex;
   align-items: center;
   margin-top: -1px; /* 상단 여백 조정 */
+  position: fixed;
+  top: 0;
+  left: 0;
   z-index: 1000; /* 사이드바 토글 버튼이 사이드바 위에 표시되도록 설정 */
+  width: 100%;
+  background-color: #333;
 }
 
 .sidebar-toggle-btn {
-  background-color: #333;
   color: #fff;
   border: none;
   cursor: pointer;
   padding: 10px;
-  font-size: 18px;
-  border-radius: 0; /* 네모 버튼으로 변경 */
+  font-size: 20px;
   transition: background-color 0.3s ease;
+  /* Updated styles */
+  position: absolute;
+  left: 10px;
+  top: 71px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  background-color: transparent;
 }
 
-.sidebar-toggle-btn.open {
-  background-color: #777; /* 버튼 열릴 때 배경색 변경 */
+/* 햄버거 아이콘을 오른쪽으로 이동 */
+.sidebar-toggle-btn i {
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
 }
 
 .sidebar-expand {
@@ -71,16 +99,16 @@ export default {
 
 .sidebar {
   width: 150px;
-  height: 74%;
+  height: 78.5%;
   background-color: #333;
   padding: 10px;
   position: fixed;
-  top: 14%;
+  top: 9.3%;
   left: 0;
-  z-index: -1;
   transition: transform 0.3s ease;
   transform: translateX(-100%);
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* 그림자 추가 */
+  pointer-events: none; /* 사이드바가 열렸을 때 포인터 이벤트를 받지 않도록 설정 */
 }
 
 .open {
@@ -105,5 +133,6 @@ export default {
 
 .content {
   transition: margin-left 0.3s ease;
+  pointer-events: auto; /* 사이드바가 열렸을 때 포인터 이벤트를 받도록 설정 */
 }
 </style>
