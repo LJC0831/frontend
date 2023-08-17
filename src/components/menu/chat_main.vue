f509a24fa8b71a8d8c3fcf0b8cabcdfac8f6dedb
leejc <leejc831@naver.com>
2023-08-18 00:10:09 +0900

    css 꾸미기

--- src/components/menu/chat_main.vue
+++ src/components/menu/chat_main.vue
@@ -5,14 +5,14 @@
       <button class="btn btn-primary" id="btn-exit" @click="exit()">나가기</button>
     </div>
     <div class="chat-room" v-if="selectedChatId === null">
-      <section>
+      <section class="chat-section">
         <div class="chat_row">
           <div class="chat_left">
-            <h1 class="fw-light">자유로운 채팅방</h1>
-            <p class="">누구나 자유롭게 채팅이 가능합니다. 욕설 및 성희롱 등 입력 시 제재 될 수 있습니다. 매너채팅 부탁드립니다 ^^</p>
+            <h1 class="chat-title">자유로운 채팅방</h1>
+            <p class="chat-description">누구나 자유롭게 채팅이 가능합니다. 욕설 및 성희롱 등 입력 시 제재 될 수 있습니다. 매너채팅 부탁드립니다 ^^</p>
             <p>
-              <input type="text" v-model="searchKeyword" @keyup.enter="search01()" placeholder="Search" />&nbsp;
-              <button class="btn btn-primary" @click="search01()">조회 </button>
+              <input type="text" v-model="searchKeyword" @keyup.enter="search01()" placeholder="Search" class="search-input" />&nbsp;
+              <button class="btn btn-primary search-button" @click="search01()">조회</button>
             </p>
           </div>
         </div>
@@ -24,13 +24,12 @@
             <div class="col" v-for="(chatRooms, index) in chatRooms" :key="index" @click="openChatRoom(chatRooms.id,chatRooms.pwd)">
               <div class="card shadow-sm">
                 <div class="card-img-top">
-                  <img v-if="chatRooms.file_no !== null" :src="chatRooms.imageUrl" alt="Chat Room Thumbnail" style="max-width: 100%; height: 225px;" />
-                  <svg v-else class="bd-placeholder-img" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">사진없음</text></svg>
+                  <img v-if="chatRooms.file_no !== null" :src="chatRooms.imageUrl" alt="Chat Room Thumbnail" class="thumbnail-image" />
+                  <svg v-else class="bd-placeholder-img" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#55595c" /><text x="40%" y="50%" fill="#eceeef" dy=".3em">사진없음</text></svg>
                 </div>
                 <div class="card-body">
                   <p class="card-text">
                     <span class="chatromm-subject">{{ chatRooms.subject }}</span>
-                    <span class="chatromm-user_id">{{ chatRooms.ins_user_id }}</span>
                   </p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
@@ -133,97 +132,49 @@ export default {
 </script>
   
 <style scoped>
-   .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
+  .chat-section {
+    background-color: #f9f9f9;
+    padding: 20px;
+    border-radius: 10px;
+  }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
+  .chat-title {
+    font-family: 'Arial', sans-serif;
+    font-size: 28px;
+    color: #333;
+    margin-bottom: 10px;
+  }
 
-      .b-example-divider {
-        width: 100%;
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
+  .chat-description {
+    font-family: 'Arial', sans-serif;
+    font-size: 16px;
+    color: #777;
+    margin-bottom: 20px;
+  }
 
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
+  .search-input {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 14px;
+    color: #333;
+    width: 200px;
+  }
 
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
+  .search-button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    background-color: #3498db;
+    font-size: 14px;
+    color: #fff;
+    cursor: pointer;
+  }
 
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
-
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-
-      .btn-bd-primary {
-        --bd-violet-bg: #712cf9;
-        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-        --bs-btn-font-weight: 600;
-        --bs-btn-color: var(--bs-white);
-        --bs-btn-bg: var(--bd-violet-bg);
-        --bs-btn-border-color: var(--bd-violet-bg);
-        --bs-btn-hover-color: var(--bs-white);
-        --bs-btn-hover-bg: #6528e0;
-        --bs-btn-hover-border-color: #6528e0;
-        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-        --bs-btn-active-color: var(--bs-btn-hover-color);
-        --bs-btn-active-bg: #5a23c8;
-        --bs-btn-active-border-color: #5a23c8;
-      }
-      .bd-mode-toggle {
-        z-index: 1500;
-      }
-
-      .chatromm-user_id{
-        float: right;
-      }
-      .chat_row{
-        margin-top: 20px;
-      }
-      .chat_left{
-        margin-left: 20px;
-      }
-      #btn-exit{
-        float: right;
-      }
-
-
-@media (max-width: 768px) {
-
-  #chat_main_img{
-        width:100%;
-      }
-}
+  .thumbnail-image {
+    max-width: 100%;
+    height: 225px;
+    object-fit: cover;
+  }
 
 </style>
