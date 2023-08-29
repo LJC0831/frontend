eb08e9c15b48ca83e38a191c1348051ca49f2122
LEEJAECHUN <leejc831@naver.com>
2023-08-29 09:17:19 +0900

    모바일 뒤로가기 액션

--- src/components/menu/chat_main.vue
+++ src/components/menu/chat_main.vue
@@ -355,7 +355,7 @@ export default {
       }
     }
   },
-  handleMobileBackButton(event) {
+  handleMobileBackButton: event => {
       // 모바일 뒤로가기 버튼이 눌렸을 때 실행할 로직
       if (this.isMobile() && this.selectedChatId !== null) {
         // 선택된 채팅이 있을 경우 exit() 메서드 실행
