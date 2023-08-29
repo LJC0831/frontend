0b83e26cc6a5655021fa9ff2eda06228f21a4070
leejc <leejc831@naver.com>
2023-08-30 00:23:17 +0900

    수정

--- src/components/main_01.vue
+++ src/components/main_01.vue
@@ -4,8 +4,8 @@
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light header-section">
       <div class="col-md-5 p-lg-5 mx-auto my-5 header-content">
         <div class="header-text">
-        <h1 class="display-4 fw-normal">Page headline</h1>
-        <p class="lead fw-normal" >This is a responsive application created with the VUE3 environment.</p>
+        <h1 class="display-4 fw-normal">Free chat application</h1>
+        <p class="lead fw-normal" >누구나 무료로 사용이 가능한 채팅 어플리케이션 입니다.</p>
         <a class="btn btn-outline-secondary" href="#">Coming soon</a>
       </div>
       </div>
