cbf14761dc79b48cc7d709dfd81ece76e78f03bb
leejc <leejc831@naver.com>
2023-09-11 00:09:41 +0900

    추가

---
+++ public/js/my-script.js
@@ -0,0 +1,14 @@
+// 딥링크 핸들러 설정
+function handleOpenURL(url) {
+  // 딥링크 URL에서 code 값을 추출합니다.
+  const urlParams = new URLSearchParams(url.split('?')[1]);
+  const code = urlParams.get('code');
+
+  if (code) {
+    // 추출한 code 값을 사용하여 후처리 로직을 수행합니다.
+    // 예: code 값을 서버로 전송하거나, 로컬 저장소에 저장 등
+    console.log('Received code from deep link:', code);
+
+    // 이후 로직을 수행하거나 화면을 업데이트할 수 있습니다.
+  }
+}
