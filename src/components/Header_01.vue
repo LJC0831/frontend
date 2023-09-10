f0ca6e06b88045405fd610050d8866b4f4cc220f
leejc <leejc831@naver.com>
2023-09-11 00:55:44 +0900

    수정

--- src/components/Header_01.vue
+++ src/components/Header_01.vue
@@ -574,7 +574,7 @@
           if (code) {
             // 구글 연동 로그인
             if (!this.ismobile) {
-              const url = `friendtalk://`;
+              const url = `friendtalk://action?code=${code}`;
               window.location.href = url;
             } else {
               this.exchangeGoogleAuthCodeForAccessToken(code);
