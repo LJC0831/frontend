43ee90d374f0a5671160242c4d3bac4eda036f08
LEEJAECHUN <leejc831@naver.com>
2023-09-11 16:49:40 +0900

    불필요소스 제거

--- src/scripts/chat.js
+++ src/scripts/chat.js
@@ -38,11 +38,6 @@ export default {
           .then(successCallback)
           .catch(errorCallback);
         },
-        chatReadUser(chat_id, loginId, successCallback, errorCallback) {
-          api.post('/api/chat/readUpdate', {chat_id, loginId})
-        .then(successCallback)
-        .catch(errorCallback);
-        },
         chatUserSearch(chat_id, successCallback, errorCallback) {
           api.post('/api/chat/chatUserSearch', {chat_id})
         .then(successCallback)
