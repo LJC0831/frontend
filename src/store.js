c7cad45f6505df52a5bea235983b4759974596be
LEEJAECHUN <leejc831@naver.com>
2023-09-15 10:04:47 +0900

    토큰시간변경

--- src/store.js
+++ src/store.js
@@ -34,6 +34,6 @@ const store = createStore({
 // 토큰 유효기간 확인 타이머 설정
 setInterval(() => {
   checkTokenExpiration();
-}, 3600000); // 60분마다 실행 (1000밀리초 * 60초 = 1분)
+}, 14400000); // 60분마다 실행 (1000밀리초 * 60초 = 1분)
 
 export default store;
