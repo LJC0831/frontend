8a5298b0b945832b966214d7ea9f564df233b944
LEEJAECHUN <leejc831@naver.com>
2023-09-18 16:53:25 +0900

    답장기능완료

--- src/components/menu/chat_01.vue
+++ src/components/menu/chat_01.vue
@@ -26,7 +26,7 @@
         <div class="message-container" >
           <div class="message-content">
           <span class="message-name">{{ message.editedName }} </span>
-          <span v-if="message.answer_message && message.answer_message !== `undefined`" class="message-answer-text">{{ message.answerUserId }} : {{ message.answer_message }} 답장</span>   
+          <span v-if="message.answer_message && message.answer_message !== `undefined`" class="message-answer-text">{{ message.answer_user_id }} : {{ message.answer_message }} 답장</span>   
             <div @click="chat_answer(message)" class="message-bubble" :class="{ 'announcement-message': message.chat_type === 'announcement' && message.chat_type !== 'search'
                                                 , 'search-message': message.chat_type === 'search'
                                                 , 'other-message': message.user_id !== this.loginUserId && message.chat_type !== 'search'
