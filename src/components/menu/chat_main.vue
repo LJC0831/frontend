8b94a4c8b0474ca27d835ccb985df54f0662d5b1
LEEJAECHUN <leejc831@naver.com>
2023-10-04 16:41:16 +0900

    소켓테스트2

--- src/components/menu/chat_main.vue
+++ src/components/menu/chat_main.vue
@@ -302,6 +302,12 @@ export default {
       } catch (error) {
         console.error("검색 오류:", error);
       }
+      this.socket = io('https://port-0-backend-nodejs-20zynm2mlk2nnlwj.sel4.cloudtype.app', {
+        withCredentials: true, // 쿠키와 인증 정보를 전송할 수 있도록 설정 (선택 사항)
+        query:{
+          userId:this.loginUserId, //로그인유저
+        }
+      });
     }, 200),
     // 방만들기
     createChatRoom(){
@@ -396,12 +402,6 @@ export default {
       this.loginUserId = decoded_Token.username;
     }
     this.search01('ALL');
-    this.socket = io('https://port-0-backend-nodejs-20zynm2mlk2nnlwj.sel4.cloudtype.app', {
-        withCredentials: true, // 쿠키와 인증 정보를 전송할 수 있도록 설정 (선택 사항)
-        query:{
-          userId:this.loginUserId, //로그인유저
-        }
-      });
   }
 };
 </script>
