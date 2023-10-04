31b00012d3918d2b40198f0f6847a7557d15a3a3
LEEJAECHUN <leejc831@naver.com>
2023-10-04 16:57:51 +0900

    원복

--- src/components/menu/chat_main.vue
+++ src/components/menu/chat_main.vue
@@ -97,7 +97,7 @@ import ChatRoom01 from './chat_01.vue';
 import loginMethods from '../../scripts/login.js';
 import chatMethods from '../../scripts/chat.js';
 import * as commons from '../../scripts/common.js';
-import io from 'socket.io-client';
+
 
 const api = axios.create({
   baseURL: "https://port-0-backend-nodejs-20zynm2mlk2nnlwj.sel4.cloudtype.app",
@@ -302,12 +302,6 @@ export default {
       } catch (error) {
         console.error("검색 오류:", error);
       }
-      this.socket = io('https://port-0-backend-nodejs-20zynm2mlk2nnlwj.sel4.cloudtype.app', {
-        withCredentials: true, // 쿠키와 인증 정보를 전송할 수 있도록 설정 (선택 사항)
-        query:{
-          userId:this.loginUserId, //로그인유저
-        }
-      });
     }, 200),
     // 방만들기
     createChatRoom(){
