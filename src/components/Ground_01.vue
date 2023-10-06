255fddf5e0152a29bfb81bbde64541a3fe2c91e1
LEEJAECHUN <leejc831@naver.com>
2023-10-06 17:34:00 +0900

    라우터 수정

--- src/components/Ground_01.vue
+++ src/components/Ground_01.vue
@@ -43,6 +43,10 @@ export default {
   },
   methods: {
     handleMenuSelected(menuName) {
+      if(menuName === 'userList') this.$router.push(`/user`)
+      else if(menuName === 'chat_main') this.$router.push(`/chat`)
+      else if(menuName === 'memoVue') this.$router.push(`/memo`)
+      else this.$router.push(`/`)
       this.selectedMenu = menuName; // 선택된 메뉴 이름으로 selectedMenu 데이터를 업데이트합니다
     },
     // toggleSidebar() {
