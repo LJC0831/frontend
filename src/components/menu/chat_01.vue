0aef4c6b4feb89ea12764aaed1bd3c91460ec480
leejc <leejc831@naver.com>
2024-01-20 00:23:14 +0900

    수정

--- src/components/menu/chat_01.vue
+++ src/components/menu/chat_01.vue
@@ -220,6 +220,9 @@
       this.socket = io('https://backendserver.shop:3000', {
       //this.socket = io('https://port-0-backend-nodejs-20zynm2mlk2nnlwj.sel4.cloudtype.app', {
         withCredentials: true, // 쿠키와 인증 정보를 전송할 수 있도록 설정 (선택 사항)
+        extraHeaders: {
+          'Access-Control-Allow-Origin': 'https://friendtalk.shop',
+        },
         query:{
           userId:this.loginUserId, //로그인유저
         }
