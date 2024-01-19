e3a11e3c2cf524555b1e0d300eacef2569aa53e9
leejc <leejc831@naver.com>
2024-01-19 22:24:39 +0900

    오류수정

--- src/components/menu/chat_01.vue
+++ src/components/menu/chat_01.vue
@@ -219,7 +219,7 @@
       // Socket.IO 클라이언트를 초기화하고 서버에 연결합니다.
       //this.socket = io('http://localhost:3000', {
       this.socket = io('https://backendserver.shop:3000', {
-        withCredentials: true, // 쿠키와 인증 정보를 전송할 수 있도록 설정 (선택 사항)
+        
         query:{
           userId:this.loginUserId, //로그인유저
         }
